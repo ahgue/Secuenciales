38aa565bbb35ad35324a3eaab1e7fcac3d108a2c
edgar.ahgue <fchinosk@hotmail.com>
2016-10-03 22:53:56 -0500

    Secuenciales
    Finished

--- Secuenciales/ViewController.swift
+++ Secuenciales/ViewController.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBAction func searchGroups(_ sender: UIButton) {
+        
+        let secuence = textView.text.characters.split(separator: " ").map(String.init)
+        print("the secuence is \(secuence)")
+        //let num = secuence.count
+        var z = 0
+      
+        for index in 0...secuence.count-2 {
+            
+            let number1 = secuence[index]
+            let number2 = secuence[index+1]
+            //aqui saco el número que esta en el indice
+            let s1 :Int? = Int(number1)
+            let s2 :Int? = Int(number2)
+            if s1! == (s2!-1) {
+                z = z+1
+            }
+            if s1! != (s2!-1) && z != 0 {
+                print("hay un grupo de \(s1!-z) hasta \(s1!)")
+                z = 0
+            }
+            if index == secuence.count-2 {
+                print("hay un grupo de \(s2!-z) hasta \(s2!)")
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
